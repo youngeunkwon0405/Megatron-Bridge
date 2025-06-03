59ba6f5288097d5c3098490da03b4ae2dc64473d
Ananth Subramaniam <ansubramania@nvidia.com>
2025-06-03 09:28:53 -0700

    Fix finetuning dataset import (#34)
    Signed-off-by: Ananth Subramaniam <ansubramania@nvidia.com>

--- nemo_lm/data/utils.py
+++ nemo_lm/data/utils.py
@@ -20,7 +20,7 @@ from megatron.core.datasets.blended_megatron_dataset_builder import BlendedMegat
 from megatron.core.datasets.blended_megatron_dataset_config import BlendedMegatronDatasetConfig
 from megatron.core.datasets.gpt_dataset import GPTDataset, MockGPTDataset
 
-from nemo_lm.data.builders import FinetuningDatasetBuilder
+from nemo_lm.data.builders.finetuning_dataset import FinetuningDatasetBuilder
 from nemo_lm.data.builders.hf_dataset import HFDatasetBuilder, HFDatasetConfig
 from nemo_lm.tokenizers.tokenizer import MegatronTokenizer
 from nemo_lm.training.config import DataloaderConfig, FinetuningDatasetConfig, GPTDatasetConfig
