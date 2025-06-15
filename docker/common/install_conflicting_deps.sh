e80315fc7935c3e761949d3364bb0d4e57500963
oliver könig <okoenig@nvidia.com>
2025-06-15 22:38:01 +0200

    build: Fix nemo dependency (#90)
    * update
    
    Signed-off-by: oliver könig <okoenig@nvidia.com>
    
    * update
    
    Signed-off-by: oliver könig <okoenig@nvidia.com>
    
    * remove marker
    
    Signed-off-by: oliver könig <okoenig@nvidia.com>
    
    * Revert "remove marker"
    
    This reverts commit e4ff621c9ea2a34c1fb1a2b1aea59d65b2b4d60b.
    
    * f
    
    Signed-off-by: oliver könig <okoenig@nvidia.com>
    
    * cleanup
    
    Signed-off-by: oliver könig <okoenig@nvidia.com>
    
    * Update manifest.json
    
    ---------
    
    Signed-off-by: oliver könig <okoenig@nvidia.com>

---
+++ docker/common/install_conflicting_deps.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -xeuo pipefail # Exit immediately if a command exits with a non-zero status
+
+# Parse command line arguments
+while [[ $# -gt 0 ]]; do
+    case $1 in
+    --export-deploy-ref)
+        EXPORT_DEPLOY_REF="$2"
+        shift 2
+        ;;
+    *)
+        echo "Unknown option: $1"
+        echo "Usage: $0 --export-deploy-ref EXPORT_DEPLOY_REF"
+        exit 1
+        ;;
+    esac
+done
+
+# Check if required arguments are provided
+if [ -z "$EXPORT_DEPLOY_REF" ]; then
+    echo "Error: --export-deploy-ref"
+    echo "Usage: $0 --export-deploy-ref EXPORT_DEPLOY_REF"
+    exit 1
+fi
+
+source ${UV_PROJECT_ENVIRONMENT}/bin/activate
+
+# Nemo-run has conflicting dependencies to export-deploy:
+# They collide on nvidia-pytriton (export-deploy) and torchx (nemo-run)
+# via urllib3.
+uv pip install --no-cache-dir --upgrade nemo_run
+
+uv pip install --no-cache-dir --upgrade nemo-export-deploy[te,trtllm]@git+https://github.com/NVIDIA-NeMo/NeMo-Export-Deploy.git@${EXPORT_DEPLOY_REF}
